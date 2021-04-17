55e04551101bd1ff905e7bc82806dee44ce9fc67
Pradipta Choudhury <72348164+pradipta18choudhury@users.noreply.github.com>
2021-04-17 10:16:02 +0530

    Create Maximum Subarray.cpp
    Runtime: 4 ms, faster than 91.53% of C++ online submissions for Maximum Subarray.
    Memory Usage: 13.1 MB, less than 84.77% of C++ online submissions for Maximum Subarray.

---
+++ Maximum Subarray.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int meh=0;
+        int msf=INT_MIN;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            meh+=nums[i];
+            if(meh<nums[i])
+                meh=nums[i];
+            if(meh>msf)
+                msf=meh;
+        }
+        return msf;
+    }
+};
+
+/******
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-105 <= nums[i] <= 105
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+******/
