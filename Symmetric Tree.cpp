055f80efc337c2d318f794203a8fa2410e400bf1
Pradipta Choudhury <72348164+pradipta18choudhury@users.noreply.github.com>
2021-04-19 23:07:42 +0530

    Create Symmetric Tree.cpp
    Runtime: 4 ms, faster than 75.41% of C++ online submissions for Symmetric Tree.
    Memory Usage: 16.3 MB, less than 45.71% of C++ online submissions for Symmetric Tree.

---
+++ Symmetric Tree.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode *p,TreeNode *q)
+    {
+        if(!p && !q)
+            return true;
+        else{
+            if(p&&q)
+            {
+                if(p->val==q->val)
+                    return helper(p->left,q->right)&&helper(p->right,q->left);
+            }
+        }
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return helper(root,root);
+    }
+};
+
+/*******
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Could you solve it both recursively and iteratively?
+Accepted
+870,225
+Submissions
+1,793,390
+*******/
