7a65908039fdc51980e8028bcc9e40085d294cfb
Pradipta Choudhury <72348164+pradipta18choudhury@users.noreply.github.com>
2021-04-24 07:40:06 +0530

    Runtime: 20 ms, faster than 56.18% of C++ online submissions for Single Number.

---
+++ Single Number.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        
+        sort(nums.begin(),nums.end());
+        int i;
+        for(i=0;i<n;i++)
+        {
+            if(n==1)
+                break;
+            else if(nums[i]!=nums[i+1])
+                break;
+            else if(nums[i]==nums[i+1])
+                i++;
+        }
+        return nums[i];
+    }
+};
+
+/******
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+******/
