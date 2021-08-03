84a8fa49dba2f80f5c1d9a34016a02bdd61a87ba
Pradipta Choudhury <72348164+pradipta18choudhury@users.noreply.github.com>
2021-08-03 11:48:03 +0530

    Create print reverse linklist.cpp

---
+++ common placement practise_q/print reverse linklist.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+void print_linkedlist_spl(node*head)
+{
+    //write your code here
+    if(head==NULL)
+    {
+        return;
+    }
+    else
+    {
+        print_linkedlist_spl(head->next);
+        cout<<head->data<<" ";
+    }
+}
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		printReverse(head);
+		cout << endl;
+	}
+	return 0;
+}
